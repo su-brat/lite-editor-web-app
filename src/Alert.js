b2d95c715a6887f6858de2d7122f0217d572d628
subrat1729 <subratpatnaik.email@gmail.com>
2022-02-21 14:56:57 +0530

    Addressed alert-box shift

--- src/Alert.js
+++ src/Alert.js
@@ -3,13 +3,15 @@ import React from "react";
 export default function Alert(props) {
   const { msg } = props;
   return (
-    msg && (
-      <div
-        className="alert alert-warning alert-dismissible fade show"
-        role="alert"
-      >
-        {msg}
-      </div>
-    )
+    <div style={{ height: "50px" }}>
+      {msg && (
+        <div
+          className="alert alert-warning alert-dismissible fade show"
+          role="alert"
+        >
+          {msg}
+        </div>
+      )}
+    </div>
   );
 }
